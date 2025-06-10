b5966e2be07c7d4653d8dc75674eb4f2a9576e2a
wjparker-js <wjparker@outlook.com>
2025-06-10 17:25:42 -0400

    feat: Enhanced blog editor with inline image upload and YouTube video embeds

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,53 @@
-module.exports = {
-  siteMetadata: {
-    title: "Blog CMS",
-    description: "A Gatsby blog with CMS functionality",
-    author: "@gatsby",
+const meta = require("./gatsby-meta-config")
+
+const siteMetadata = {
+  title: meta.title,
+  description: meta.description,
+  author: meta.author,
+  siteUrl: meta.siteUrl,
+  lang: meta.lang,
+  utterances: {
+    repo: meta.utterances,
   },
+  postTitle: "All",
+  menuLinks: [
+    {
+      link: "/",
+      name: "Home",
+    },
+    {
+      link: "/about/",
+      name: "About",
+    },
+    {
+      link: meta.links.github,
+      name: "Github",
+    },
+  ],
+}
+
+module.exports = {
+  siteMetadata,
   plugins: [
+    "gatsby-plugin-react-helmet",
     "gatsby-plugin-typescript",
     "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "src",
         path: `${__dirname}/src`,
+        ignore: [`**/*.d.ts`],
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images", 
+        path: `${__dirname}/src/images`,
       },
     },
   ],
